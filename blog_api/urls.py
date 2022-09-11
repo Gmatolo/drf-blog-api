2ff4839672aa36c9ab21e2c5aee127bf13899c69
gmatolo <mulimatolo@gmail.com>
2022-09-11 19:10:29 +0000

    Adding in commands.txt

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import PostList, PostDetail
+
+from .views import PostDetail, PostList
 
 app_name = 'blog_api'
 
 urlpatterns = [
     # single api endpoint
-    path('<int:pk>/', PostDetail.as_view, name='detailcreate'), 
+    path('<int:pk>/', PostDetail.as_view(), name='detailcreate'), 
     # list all endpoints in db
     path('', PostList.as_view(), name='listcreate')
 ]
